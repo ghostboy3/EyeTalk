091d17d5b4eeabc2944b962d456cde1bdd0a0d99
ghostboy3 <charlie.to.luu@gmail.com>
2023-09-17 07:30:28 -0400

    updated eye tracking code

--- message.py
+++ message.py
@@ -5,7 +5,9 @@ import time
 
 import adhawkapi
 import adhawkapi.frontend
-from tkTest import click, sendSMS, speak_button_text
+
+import mouse
+
 #sglobal gaze = None
 
 class FrontendData:
@@ -29,13 +31,11 @@ class FrontendData:
     def __init__(self):
         # Instantiate an API object
         # TODO: Update the device name to match your device
-        self._api = adhawkapi.frontend.FrontendApi(ble_device_name='ADHAWK MINDLINK-305')
-
-    
+        self._api = adhawkapi.frontend.FrontendApi(ble_device_name='ADHAWK MINDLINK-282')
 
         # Tell the api that we wish to receive eye tracking data stream
         # with self._handle_et_data as the handler
-        self._api.register_stream_handler(adhawkapi.PacketType.EYETRACKING_STREAM, self.handle_et_data)#_handle_et_data)
+        self._api.register_stream_handler(adhawkapi.PacketType.EYETRACKING_STREAM, self._handle_et_data)#_handle_et_data)
 
         # Tell the api that we wish to tap into the EVENTS stream
         # with self._handle_events as the handler
@@ -53,17 +53,37 @@ class FrontendData:
     #@staticmethod
     #def quadrant_handler(event_type, *args, xvec,yvec,zvec,x0,y0,factored_length,factored_width):
 
-        
-
+    
 
     @staticmethod
-    def handle_et_data(et_data: adhawkapi.EyeTrackingStreamData):
+    def _handle_et_data(et_data: adhawkapi.EyeTrackingStreamData):
         ''' Handles the latest et data '''
-        time.sleep(400) #UPDATED
         if et_data.gaze is not None:
             xvec, yvec, zvec, vergence = et_data.gaze
             print(f'Gaze x={xvec:.2f},y={yvec:.2f},z={zvec:.2f},vergence={vergence:.2f}')
 
+        # if et_data.eye_center is not None:
+        #     if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
+        #         rxvec, ryvec, rzvec, lxvec, lyvec, lzvec = et_data.eye_center
+        #         print(f'Eye center: Left=(x={lxvec:.2f},y={lyvec:.2f},z={lzvec:.2f}) '
+        #               f'Right=(x={rxvec:.2f},y={ryvec:.2f},z={rzvec:.2f})')
+
+        # if et_data.pupil_diameter is not None:
+        #     if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
+        #         rdiameter, ldiameter = et_data.pupil_diameter
+        #         print(f'Pupil diameter: Left={ldiameter:.2f} Right={rdiameter:.2f}')
+
+        # if et_data.imu_quaternion is not None:
+        #     if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
+        #         x, y, z, w = et_data.imu_quaternion
+        #         print(f'IMU: x={x:.2f},y={y:.2f},z={z:.2f},w={w:.2f}')
+        # #gaze = et_data.gaze
+    
+
+        # if event_type == adhawkapi.Events.BLINK:
+        #     print("YOU CANNOT MISS THIS MESSAGE UNLESS THERES AN ERROR!")
+        #     duration  = args[0]
+        #     print(duration)
             if FrontendData.i == False:
                 print("YOU CANNOT MISS THIS MESSAGE UNLESS THERES AN ERROR!")
                 # if duration  >= 5 and duration <= 10:
@@ -100,30 +120,48 @@ class FrontendData:
         #     print(args)
         #     print(f'Got blink: {timestamp} {duration}')
 
-        if event_type == adhawkapi.Events.EYE_CLOSED:
-            eye_idx = args[0]
-            print("durationnnnnnnnnnnnnnnnnnnnnnnnnn" + str(eye_idx))
-            if eye_idx >3:
+        if event_type == adhawkapi.Events.BLINK:
+            duration = args[0]
+            print(duration)
+
+            # '''  <--------------------------------------------->'''
+            # start_time = time.time()
+            # while time.time() - start_time < 2:
+            #     if event_type == adhawkapi.Events.BLINK:
+            #         '''  <--------------------------------------------->'''
+
                 #Quadrants 1 and 4``
-                if (FrontendData.xvec>(FrontendData.x0-(FrontendData.factored_length/3))&FrontendData.xvec<(FrontendData.x0-(FrontendData.factored_length/6))):
-                    if (FrontendData.yvec>FrontendData.y0)&(FrontendData.yvec<(FrontendData.y0+(FrontendData.factored_width/2))):
-                        print("Quadrant 1")
-                    else:
-                        print("Quadrant 4")
+            if (FrontendData.xvec>(FrontendData.x0-(FrontendData.factored_length/3))&FrontendData.xvec<(FrontendData.x0-(FrontendData.factored_length/6))):
+                if (FrontendData.yvec>FrontendData.y0)&(FrontendData.yvec<(FrontendData.y0+(FrontendData.factored_width/2))):
+                    print("Quadrant 1")
+                    mouse.move(234, 209, absolute=False, duration=0.5)
+                    mouse.click('left')
+                else:
+                    print("Quadrant 4")
+                    mouse.move(246, 644, absolute=False, duration=0.5)
+                    mouse.click('left')
 
-                #Quadrant 2 and 5
-                elif (FrontendData.xvec>(FrontendData.x0-(FrontendData.factored_length/6))&FrontendData.xvec<(FrontendData.x0-(FrontendData.factored_length/6))):
-                    if (FrontendData.yvec>FrontendData.y0)&(FrontendData.yvec<(FrontendData.y0+(FrontendData.factored_width/2))):
-                        print("Quadrant 2")
-                    else:
-                        print("Quadrant 5")
+            #Quadrant 2 and 5
+            elif (FrontendData.xvec>(FrontendData.x0-(FrontendData.factored_length/6))&FrontendData.xvec<(FrontendData.x0-(FrontendData.factored_length/6))):
+                if (FrontendData.yvec>FrontendData.y0)&(FrontendData.yvec<(FrontendData.y0+(FrontendData.factored_width/2))):
+                    print("Quadrant 2")
+                    mouse.move(729, 226, absolute=False, duration=0.5)
+                    mouse.click('left')
+                else:
+                    print("Quadrant 5")
+                    mouse.move(742, 645, absolute=False, duration=0.5)
+                    mouse.click('left')
 
-                #Quadrant 3 and 6
-                elif (FrontendData.xvec>(FrontendData.x0+(FrontendData.factored_length/6))&FrontendData.xvec<(FrontendData.x0+(FrontendData.factored_length/6))):
-                    if (FrontendData.yvec>FrontendData.y0)&(FrontendData.yvec<(FrontendData.y0+(FrontendData.factored_width/2))):
-                        print("Quadrant 3")
-                    else:
-                        print("Quadrant 6")
+            #Quadrant 3 and 6
+            elif (FrontendData.xvec>(FrontendData.x0+(FrontendData.factored_length/6))&FrontendData.xvec<(FrontendData.x0+(FrontendData.factored_length/6))):
+                if (FrontendData.yvec>FrontendData.y0)&(FrontendData.yvec<(FrontendData.y0+(FrontendData.factored_width/2))):
+                    print("Quadrant 3")
+                    mouse.move(1252, 219, absolute=False, duration=0.5)
+                    mouse.click('left')
+                else:
+                    print("Quadrant 6")
+                    mouse.move(1262, 642, absolute=False, duration=0.5)
+                    mouse.click('left')
                         
         if event_type == adhawkapi.Events.EYE_CLOSED:
             eye_idx = args[0]
@@ -135,35 +173,35 @@ class FrontendData:
             print(f'Eye Open: {timestamp} {eye_idx}')
 
 
-    @staticmethod
-    def quadrant_handler(event_type, *args, xvec,yvec,zvec,x0,y0,factored_length,factored_width):
+    # @staticmethod
+    # def quadrant_handler(event_type, *args, xvec,yvec,zvec,x0,y0,factored_length,factored_width):
 
-        if event_type == adhawkapi.Events.BLINK:
-            duration = args[0]
-            print(duration)
-            if duration >3000:
-                #Quadrants 1 and 4``
-                if (xvec>(x0-(factored_length/3))&xvec<(x0-(factored_length/6))):
-                    if (yvec>y0)&(yvec<(y0+(factored_width/2))):
-                        print("Quadrant 1")
-                    else:
-                        print("Quadrant 4")
+    #     if event_type == adhawkapi.Events.BLINK:
+    #         duration = args[0]
+    #         print(duration)
+    #         #if duration >3000:
+    #         #Quadrants 1 and 4
+    #         if (xvec>(x0-(factored_length/3))&xvec<(x0-(factored_length/6))):
+    #             if (yvec>y0)&(yvec<(y0+(factored_width/2))):
+    #                 print("Quadrant 1")
+    #             else:
+    #                 print("Quadrant 4")
 
-                #Quadrant 2 and 5
-                elif (xvec>(x0-(factored_length/6))&xvec<(x0-(factored_length/6))):
-                    if (yvec>y0)&(yvec<(y0+(factored_width/2))):
-                        print("Quadrant 2")
-                    else:
-                        print("Quadrant 5")
+    #         #Quadrant 2 and 5
+    #         elif (xvec>(x0-(factored_length/6))&xvec<(x0-(factored_length/6))):
+    #             if (yvec>y0)&(yvec<(y0+(factored_width/2))):
+    #                 print("Quadrant 2")
+    #             else:
+    #                 print("Quadrant 5")
 
-                #Quadrant 3 and 6
-                elif (xvec>(x0+(factored_length/6))&xvec<(x0+(factored_length/6))):
-                    if (yvec>y0)&(yvec<(y0+(factored_width/2))):
-                        print("Quadrant 3")
-                    else:
-                        print("Quadrant 6")
+    #         #Quadrant 3 and 6
+    #         elif (xvec>(x0+(factored_length/6))&xvec<(x0+(factored_length/6))):
+    #             if (yvec>y0)&(yvec<(y0+(factored_width/2))):
+    #                 print("Quadrant 3")
+    #             else:
+    #                 print("Quadrant 6")
 
-
+    
     def _handle_tracker_connect(self):
         print("Tracker connected")
         self._api.set_et_stream_rate(60, callback=lambda *args: None)
@@ -182,10 +220,8 @@ class FrontendData:
     def _handle_tracker_disconnect(self):
         print("Tracker disconnected")
 
-
     #def _calibration(event_type, timestamp, *args, et_data: adhawkapi.EyeTrackingStreamData):
        
-
     def field_of_vision_screen():
         length =0.346
         width= 0.194
